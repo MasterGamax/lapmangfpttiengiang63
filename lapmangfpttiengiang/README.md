7c20a3844eec6b43a7cc1a1a0481dd1a13e65a0a
MasterGamax <134747169+MasterGamax@users.noreply.github.com>
2024-07-03 19:25:03 +0700

    commit

--- lapmangfpttiengiang/README.md
+++
@@ -1,5 +0,0 @@
-# lapmangfpttiengiang
-Vietnamese
-
-# Server bán hàng về các sản phẩm của FPT
-Có thắc mắc vui lòng liên hệ Email: truongdandoan4567@gmail.com
--- lapmangfpttiengiang/src/pages/ProductDetail.js
+++
@@ -1,8 +0,0 @@
-export const fetchProducts = async () => {
-    // Placeholder for actual API call
-    return [
-        { id: 1, name: 'Product 1', price: '$100', image: '/assets/images/product1.jpg' },
-        { id: 2, name: 'Product 2', price: '$200', image: '/assets/images/product2.jpg' },
-        // Add more products here
-    ];
-};
