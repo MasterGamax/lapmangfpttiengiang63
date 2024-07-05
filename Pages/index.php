b9732d8abc2bb880f245d4b6f3583d324ec7452b
MasterGamax <134747169+MasterGamax@users.noreply.github.com>
2024-07-05 09:56:09 +0700

    Chỉnh sửa ảnh baner

--- Pages/index.php
+++ Pages/index.php
@@ -5,17 +5,13 @@ include "../Template/header.php";
     <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="../Assets/img/Camera-fpt-banner-1024x379-1.png" class="d-block w-100" alt="">
+            <img src="../Assets/img/Camera-fpt-banner-1024x379-1.png" class="d-block" alt="">
         </div>
         <div class="carousel-item">
-            <img src="../Assets/img/internet-fpt-vung-tau-1-1536x476.webp" class="d-block w-100" alt="">
-        </div>
-        <div class="carousel-item">
-            <img src="https://via.placeholder.com/1348x400" class="d-block w-100" alt="">
+            <img src="../Assets/img/internet-fpt-vung-tau-1-1536x476.webp" class="d-block" alt="">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
--- Template/header.php
+++ Template/header.php
@@ -13,8 +13,8 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
     <link rel="stylesheet" href="../Assets/css/style.css">
+    <link rel="stylesheet" href="../Assets/css/baner.css">
 </head>
 
 <body>
